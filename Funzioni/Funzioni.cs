e6d5a220762484cce0925623e1b4353f8be86b39
kevin trentini <trentinikevin6@gmail.com>
2020-12-02 14:06:25 +0100

    Add project files.

---
+++ Funzioni/Funzioni.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Funzioni
+{
+    public class Funzioni
+    {
+        public static int Input()
+        {
+            Console.WriteLine("Inserisci un numero intero");
+            int a = Convert.ToInt32(Console.ReadLine());
+            return a;
+        }
+        public static void OutputDivisori(int a)
+        {
+            int cont = 1;
+            int max = a / 2;
+            while (cont <= max)
+            {
+                if (a % cont == 0)
+                    Console.WriteLine(cont);
+                cont++;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
+
---
+++ Prova funzioni/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Funzioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = Funzioni.Input();
+            Funzioni.OutputDivisori(n1);
+        }
+    }
+}
